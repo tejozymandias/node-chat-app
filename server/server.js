893f399f73142e35bbb81440baa2d6d7db82bf01
tejozymandias <tej.ozymandias@gmail.com>
2019-10-01 17:21:06 +0530

    socket testing

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
+
+
 
 const publicPath = path.join(__dirname,'../public');
-
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
+io.on('connection', (socket)=>{
+    console.log('new user connected');
+    socket.on('disconnect',()=>{
+        console.log('user was Disconnected');
+    });
+    });
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT || 5000);
+server.listen(process.env.PORT || 5000);
