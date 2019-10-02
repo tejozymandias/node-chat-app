37d15e69542195d1cc5be4aa2f74f41920a053e9
tejozymandias <tej.ozymandias@gmail.com>
2019-10-02 15:02:13 +0530

    minor changes

--- public/js/index.js
+++ public/js/index.js
@@ -5,17 +5,12 @@
 
         socket.on('connect',()=>{
             console.log('Connected to Server');
-
-            socket.emit('createEmail',{
-                to:'jen@example.com',
-                text:'hey'
-            });
-        });
+                  });
 
         socket.on('disconnect',()=>{
             console.log('Disconnected Server');
         });
 
-        socket.on('newEmail',(email)=>{
-            console.log('New EMail',email);
+        socket.on('newMessage',(message)=>{
+            console.log('newMessage',message);
         });
--- server/server.js
+++ server/server.js
@@ -16,14 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log('new user connected');
 
-socket.emit('newMessage',{
-    from:'mike@example.com',
-    text:'Hey, what is going on',
-    createAt:123
-});
-
     socket.on('createMessage',(message)=>{
         console.log('createMessage', message);
+        io.emit('newMessage',{
+           from: message.from,
+           text: message.text,
+           createdAt:new Date().getTime()        
+        });
     });
 
     socket.on('disconnect',()=>{
